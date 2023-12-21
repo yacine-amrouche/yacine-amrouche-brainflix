836212fd140e264c3507a6f682b5577f264ccfd5
yacine <adam_sfez@otmail.com>
2023-12-20 21:36:38 -0500

    making a video section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
 import Header from './components/header/Header';
-
+import Video from './components/video/Video';
 function App() {
   return (
    <>
    <Header />
+   <Video />
    </>
   );
 }
